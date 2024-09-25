90ab5ce368afd3105506aa64590193dbde7dc328
Wasim-Afreeth-Z <wasimafreeth36@gmail.com>
2024-09-25 13:42:04 +0530

    c

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     SECRETKEY : 'ecommerce-website-secretkey',
-    BASEURL:'https://e-commerce-website-backend-3vfl.onrender.com'
+    BASEURL:'https://e-commerce-website-backend-8d5y.onrender.com'
     // BASEURL:'http://localhost:5000'
 }
